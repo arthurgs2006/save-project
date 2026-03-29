6f1c38e3ad2c1b1244821a9ba566e105276f09a7
arthurgs2006 <arthurgonsal@hotmail.com>
2026-03-29 13:07:33 -0300

    finishing config of local server.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 // Configuração para escolher o servidor
-export const USE_LOCAL_SERVER = true; // Mude para false para usar o servidor externo
+export const USE_LOCAL_SERVER = false; // Mude para false para usar o servidor externo
+// ...
+export const BASE_URL = "https://database-save-app.onrender.com";
 
-export const BASE_URL = USE_LOCAL_SERVER
-  ? "http://localhost:3001"
-  : "https://database-save-app.onrender.com";
+// USAR SERVIDOR LOCAL:
+// "http://localhost:3001"
